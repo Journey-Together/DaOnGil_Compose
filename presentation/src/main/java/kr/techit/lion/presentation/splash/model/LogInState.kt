32d9308570d357027912f0cea2a01f61428b531e
chanho0908 <uni070@naver.com>
2025-01-31 17:59:06 +0900

    [Feat] #5 기존 Login, OnBoarding, Concern, Splash View 제거

--- presentation/src/main/java/kr/techit/lion/presentation/splash/model/LogInState.kt
+++
@@ -1,7 +0,0 @@
-package kr.techit.lion.presentation.splash.model
-
-sealed class LogInStatus {
-    data object Checking : LogInStatus()
-    data object LoggedIn : LogInStatus()
-    data object LoginRequired : LogInStatus()
-}
