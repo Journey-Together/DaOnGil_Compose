cc090d4be4f89156fbdf5c8fe96e3f25ebcdcc5b
chanho0908 <uni070@naver.com>
2025-02-06 17:22:24 +0900

    [Feat] #5 최초 유저 판별 데이터 Boolean Type으로 수정 후 Repository maping code 제거

--- data/src/main/java/kr/techit/lion/data/repository/ActivationRepositoryImpl.kt
+++ data/src/main/java/kr/techit/lion/data/repository/ActivationRepositoryImpl.kt
@@ -1,8 +1,6 @@
 package kr.techit.lion.data.repository
 
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.map
-import kr.techit.lion.datastore.Activation
 import kr.techit.lion.datastore.ActivationDataSource
 import kr.techit.lion.domain.repository.ActivationRepository
 import javax.inject.Inject
@@ -12,12 +10,7 @@ internal class ActivationRepositoryImpl @Inject constructor(
 ) : ActivationRepository {
 
     override val activation: Flow<Boolean>
-        get() = activationDataSource.activation.map { activation ->
-            when (activation) {
-                is Activation.Activate -> true
-                is Activation.DeActivate -> false
-            }
-        }
+        get() = activationDataSource.activation
 
     override suspend fun saveUserActivation() {
         activationDataSource.saveActivation()
