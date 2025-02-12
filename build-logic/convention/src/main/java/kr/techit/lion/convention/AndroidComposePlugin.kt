1cf3ee62bdac4ec0a450a93defc83b5ba6072fa5
chanho0908 <uni070@naver.com>
2025-02-12 09:57:30 +0900

    [Feat] Compose Plugin에 Android Library Plugin 추가

--- build-logic/convention/src/main/java/kr/techit/lion/convention/AndroidComposePlugin.kt
+++ build-logic/convention/src/main/java/kr/techit/lion/convention/AndroidComposePlugin.kt
@@ -1,7 +1,11 @@
 package kr.techit.lion.convention
 
 import com.android.build.gradle.LibraryExtension
+import kr.techit.lion.convention.extension.Plugins
+import kr.techit.lion.convention.extension.applyPlugins
 import kr.techit.lion.convention.extension.configureComposeAndroid
+import kr.techit.lion.convention.extension.configureKotlinAndroid
+import kr.techit.lion.convention.extension.configureKotlinCoroutine
 import kr.techit.lion.convention.extension.getLibrary
 import kr.techit.lion.convention.extension.implementation
 import kr.techit.lion.convention.extension.libs
@@ -10,8 +14,12 @@ import org.gradle.kotlin.dsl.dependencies
 
 class AndroidComposePlugin : BuildLogicConventionPlugin(
   block = {
+      applyPlugins(Plugins.ANDROID_LIBRARY)
+
       extensions.configure<LibraryExtension> {
           configureComposeAndroid(this)
+          configureKotlinAndroid(this)
+          configureKotlinCoroutine(this)
       }
 
       dependencies {
